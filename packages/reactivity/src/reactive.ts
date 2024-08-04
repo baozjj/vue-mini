bea768c41be50bda69006ab39d889e6f4fe0e3a9
baozj <1670370148@qq.com>
2024-08-04 22:30:20 +0800

    feat: add isReactive

--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -3,6 +3,10 @@ import { mutableHandlers } from './baseHandlers'
 
 export const reactiveMap = new WeakMap<object, any>()
 
+export const enum ReactiveFlags {
+  IS_REACTIVE = '__v_isReactive'
+}
+
 export function reactive(target: object) {
   return createReactiveObject(target, mutableHandlers, reactiveMap)
 }
@@ -16,6 +20,7 @@ function createReactiveObject(
   if (existingProxy) return existingProxy
 
   const proxy = new Proxy(target, baseHandlers)
+  proxy[ReactiveFlags.IS_REACTIVE] = true
   proxyMap.set(target, proxy)
 
   return proxy
@@ -23,4 +28,8 @@ function createReactiveObject(
 
 export const toReactive = <T extends unknown>(value: T): T => {
   return isObject(value) ? reactive(value as object) : value
+}
+
+export const isReactitve = (value): boolean => {
+  return !!(value && value[ReactiveFlags.IS_REACTIVE])
 }
