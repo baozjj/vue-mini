36cd7b17bad1af2568f7b7be48a5c834e40aa196
baozj <1670370148@qq.com>
2024-08-07 22:19:57 +0800

    feat: add isArray

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -12,3 +12,5 @@ export const isFunction = (val: unknown): val is Function => {
 export const extend = Object.assign
 
 export const EMPTY_OBJ: { readonly [key: string]: any } = {}
+
+export const isArray = Array.isArray
